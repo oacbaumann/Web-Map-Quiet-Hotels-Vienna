26ab1548186af1e4510458ea57a7b3689c7a945c
oacbaumann <93262112+oacbaumann@users.noreply.github.com>
2022-06-15 18:19:41 +0200

    Initial commit

---
+++ data/hotels_8.js
@@ -0,0 +1,12 @@
+var json_hotels_8 = {
+"type": "FeatureCollection",
+"name": "hotels_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Mariahilf" }, "geometry": { "type": "Point", "coordinates": [ 16.3543674, 48.1996283 ] } },
+{ "type": "Feature", "properties": { "NAME": "Hotel Kärntnerhof" }, "geometry": { "type": "Point", "coordinates": [ 16.3761096, 48.2101672 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mailberger Hof" }, "geometry": { "type": "Point", "coordinates": [ 16.3726969, 48.2042262 ] } },
+{ "type": "Feature", "properties": { "NAME": "Römischer Kaiser" }, "geometry": { "type": "Point", "coordinates": [ 16.3725344, 48.2040742 ] } },
+{ "type": "Feature", "properties": { "NAME": "DO&CO Hotel" }, "geometry": { "type": "Point", "coordinates": [ 16.3716316, 48.208374 ] } }
+]
+}
